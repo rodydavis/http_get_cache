469b7ed5e4dd600c68a0ee115c1c213be4ac4946
Rody Davis <rody.davis.jr@gmail.com>
2024-07-18 17:31:38 +0100

    Update flutter_http_cache.dart

--- lib/flutter_http_cache.dart
+++ lib/flutter_http_cache.dart
@@ -1,7 +1,5 @@
 library flutter_http_cache;
 
-export 'src/database/database.dart';
-export 'src/database/connection/connect.dart';
 export 'src/client/http_client.dart';
 export 'src/http_get_cache.dart';
 export 'src/store/base.dart';
